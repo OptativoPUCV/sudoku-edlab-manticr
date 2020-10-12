fce9d645fd432c2598381bb7b4fe425f9972d957
repl.it user <replituser@example.com>
2020-10-12 17:52:57 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -51,7 +51,7 @@ int is_valid(Node* n){
 List* get_adj_nodes(Node* n){
     List* list=createList();
     int i,j;
-    int cont = 0;
+    int cont = 1;
     for(i = 0 ; i < 9 ; i++)
     {
       for(j = 0 ; j < 9 ; j++)
