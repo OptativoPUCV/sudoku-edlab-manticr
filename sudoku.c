0b2b4aba248d3dcb1baf132f07ffc4426571af0f
repl.it user <replituser@example.com>
2020-10-13 16:10:04 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -138,7 +138,7 @@ List* get_adj_nodes(Node* n)
           {
             Node* node = copy(n);
             node->sudo[i][j] = k;
-            if(is_valid(n))
+            if(is_valid(node))
             {
               Node* adj = createNode();
               pushBack(list, adj);
