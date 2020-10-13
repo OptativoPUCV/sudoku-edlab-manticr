646302b689103e983af744ca2ea5f4dc30805deb
repl.it user <replituser@example.com>
2020-10-13 15:58:00 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -132,17 +132,13 @@ List* get_adj_nodes(Node* n)
   List* list=createList();
   for(int i = 0 ; i < 9 ; i++)
     for(int j = 0 ; j < 9 ; j++)
-        if(n->sudo[i][j] == '\0')
+        if(n->sudo[i][j] == 0)
         {
           for(int k = 1 ; k < 10 ; k++)
           {
             Node* node = copy(n);
             node->sudo[i][j] = k;
-            if(is_valid(node))
-            {
-              Node* adj = createNode();
-              pushBack(list, adj);
-            }
+            pushBack(list, node);
           }
         } 
   return list;
