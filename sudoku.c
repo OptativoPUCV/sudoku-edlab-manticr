8640f27b270d0af88cdb65105e697c591327993f
repl.it user <replituser@example.com>
2020-10-13 16:29:45 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -136,12 +136,9 @@ List* get_adj_nodes(Node* n)
         {
           for(int k = 1 ; k < 10 ; k++)
           {
-            if(is_valid(n))
-            {
-              Node* node = copy(n);
-              node->sudo[i][j] = k;
-              if(is_valid(node)) pushBack(list, node);
-            }
+            Node* node = copy(n);
+            node->sudo[i][j] = k;
+            if(is_valid(node)) pushBack(list, node);
           }
           n->sudo[i][j] = 0;
           return list;
